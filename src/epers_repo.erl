f19686772dbae71f3b5f6db1822c922e38f55d5d
Marcelo Gornstein <marcelo@inakanetworks.com>
2012-10-14 20:48:46 -0300

    more doc added for eper_repo

--- src/epers_repo.erl
+++ src/epers_repo.erl
@@ -83,28 +83,49 @@ delete(Name, DocName, Id) ->
 delete_all(Name, DocName) ->
   gen_server:call(Name, {delete_all, DocName}).
 
+%% @doc Finds documents that match the given conditions in the given
+%% repository name.
+-spec find_by(
+  atom(), epers_schema_name(), proplists:proplist(),
+  pos_integer(), pos_integer()
+) -> [#epers_doc{}].
 find_by(Name, DocName, Conditions, Limit, Offset) ->
   gen_server:call(Name, {find_by, DocName, Conditions, Limit, Offset}).
 
+%% @doc Finds documents that match the given conditions in the given
+%% repository name.
+-spec find_by(
+  atom(), epers_schema_name(), proplists:proplist()
+) -> [#epers_doc{}].
 find_by(Name, DocName, Conditions) ->
   gen_server:call(Name, {find_by, DocName, Conditions}).
 
+%% @doc Calls a custom function in the given repository name.
+-spec call(atom(), epers_schema_name(), atom(), [term()]) -> term().
 call(Name, DocName, Function, Args) ->
   gen_server:call(Name, {call, DocName, Function, Args}).
 
+%% @doc Creates the schema of the given docs in the given repository name.
+-spec create_schema(atom(), #epers_schema{}) -> ok.
 create_schema(Name, #epers_schema{}=Schema) ->
   gen_server:call(Name, {create_schema, Schema}).
 
+%% @doc Starts and links a new process for the given repo implementation.
+-spec start_link(atom(), module(), [term()]) -> {ok, pid()}.
 start_link(Name, Module, Options) ->
   gen_server:start_link({local, Name}, ?MODULE, [Module,Options], []).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% gen_server stuff.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc Called by start_link.
+-spec init([term()]) -> {ok, #state{}}.
 init([Module, Options]) ->
   {ok, HState} = Module:init(Options),
   {ok, #state{handler=Module, handler_state=HState}}.
 
+%% @doc handles calls.
+-spec handle_call(term(), pid(), #state{}) -> term().
 handle_call(
   {persist, #epers_doc{name=DocName}=Doc}, _From,
   #state{handler=Handler,handler_state=HState}=State

7e2dacb13309810b571475a011cc2bf4251ea7a7
cabol <candres.bolanos@gmail.com>
2015-04-23 15:07:09 -0500

    Dialyzed.

--- src/sumo_store_riak.erl
+++ src/sumo_store_riak.erl
@@ -296,7 +296,7 @@ delete_map(Conn, Bucket, Key, Opts) ->
   riakc_pb_socket:delete(Conn, Bucket, Key, Opts).
 
 -spec update_map(
-  connection(), bucket(), key(), riakc_map:crdt_map(), options()
+  connection(), bucket(), key() | undefined, riakc_map:crdt_map(), options()
 ) ->
   ok | {ok, Key::binary()} | {ok, riakc_datatype:datatype()} |
   {ok, Key::binary(), riakc_datatype:datatype()} | {error, term()}.

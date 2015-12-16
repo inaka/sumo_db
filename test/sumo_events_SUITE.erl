10293c6cf8bdc52944508a35cd5221aa70265327
Harenson Henao <harenson@gmail.com>
2015-12-16 13:18:28 -0500

    [#218] Remove unneeded code from sumo_events_SUITE and fix elasticsearch events manager

--- test/sumo_events_SUITE.erl
+++ test/sumo_events_SUITE.erl
@@ -1,11 +1,6 @@
 -module(sumo_events_SUITE).
 
--export([
-         all/0,
-         init_per_suite/1,
-         end_per_suite/1,
-         init_per_testcase/2
-        ]).
+-export([all/0]).
 
 -export([events_manager_supervisor_running/1]).
 
@@ -16,31 +11,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec all() -> [atom()].
-all() ->
-  Exports = ?MODULE:module_info(exports),
-  ExcludedFunctions = [module_info,
-                       all,
-                       test,
-                       init_per_suite,
-                       init_per_testcase,
-                       end_per_suite,
-                       find_by_sort,
-                       find_all_sort
-                      ],
-  [F || {F, _} <- Exports, not lists:member(F, ExcludedFunctions)].
-
--spec init_per_suite(config()) -> config().
-init_per_suite(Config) ->
-  sumo_test_utils:start_apps(),
-  Config.
-
-init_per_testcase(_, Config) ->
-  Config.
-
--spec end_per_suite(config()) -> config().
-end_per_suite(Config) ->
-  Config.
-
+all() -> [events_manager_supervisor_running].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Common test
@@ -61,8 +32,3 @@ events_manager_supervisor_running(_Config) ->
   lists:foreach(Fun, sumo_test_utils:all_people()),
 
   {comment, ""}.
-
-
-
-
-

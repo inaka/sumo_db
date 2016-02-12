4cecb74446ecda8718f69b89d7d2fa9a656b111b
Harenson Henao <harenson@gmail.com>
2016-02-12 15:26:06 -0500

    [#201] Add store utils module

---
+++ src/sumo_store_utils.erl
@@ -0,0 +1,63 @@
+%%% Set of useful functions for all the stores
+-module(sumo_store_utils).
+
+-export([to_bin/1, to_atom/1, to_list/1, to_int/1, to_float/1]).
+
+to_bin(Data) when is_integer(Data) ->
+  integer_to_binary(Data);
+to_bin(Data) when is_float(Data) ->
+  float_to_binary(Data);
+to_bin(Data) when is_atom(Data) ->
+  atom_to_binary(Data, utf8);
+to_bin(Data) when is_list(Data) ->
+  iolist_to_binary(Data);
+to_bin(Data) when is_pid(Data); is_reference(Data); is_tuple(Data) ->
+  integer_to_binary(erlang:phash2(Data));
+to_bin(undefined) -> <<>>;
+to_bin(Data) ->
+  Data.
+
+to_atom(Data) when is_binary(Data) ->
+  binary_to_atom(Data, utf8);
+to_atom(Data) when is_list(Data) ->
+  list_to_atom(Data);
+to_atom(Data) when is_pid(Data); is_reference(Data); is_tuple(Data) ->
+  list_to_atom(integer_to_list(erlang:phash2(Data)));
+to_atom(Data) ->
+  Data.
+
+to_list(Data) when is_binary(Data) ->
+  binary_to_list(Data);
+to_list(Data) when is_integer(Data) ->
+  integer_to_list(Data);
+to_list(Data) when is_float(Data) ->
+  float_to_list(Data);
+to_list(Data) when is_atom(Data) ->
+  atom_to_list(Data);
+to_list(Data) when is_pid(Data); is_reference(Data); is_tuple(Data) ->
+  integer_to_list(erlang:phash2(Data));
+to_list(undefined) -> "";
+to_list(Data) ->
+  Data.
+
+to_int(Data) when is_binary(Data) ->
+  binary_to_integer(Data);
+to_int(Data) when is_list(Data) ->
+  list_to_integer(Data);
+to_int(Data) when is_float(Data) ->
+  trunc(Data);
+to_int(Data) when is_pid(Data); is_reference(Data); is_tuple(Data) ->
+  erlang:phash2(Data);
+to_int(Data) ->
+  Data.
+
+to_float(Data) when is_binary(Data) ->
+  binary_to_float(Data);
+to_float(Data) when is_list(Data) ->
+  list_to_float(Data);
+to_float(Data) when is_integer(Data) ->
+  Data / 1;
+to_float(Data) when is_pid(Data); is_reference(Data); is_tuple(Data) ->
+  erlang:phash2(Data) / 1;
+to_float(Data) ->
+  Data.

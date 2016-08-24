9867bda028a60f2204a2656279c5685850d8727b
Felipe Ripoll <ferigis@gmail.com>
2016-08-24 10:22:07 -0600

    [#257] find_test_helper adapted to last changes in sumo_db

--- src/adapter_test_helpers/sumo_find_test_helper.erl
+++ src/adapter_test_helpers/sumo_find_test_helper.erl
@@ -15,49 +15,53 @@
 
 -spec find_by_sort(config()) -> ok.
 find_by_sort(Config) ->
-  {_, Module} = lists:keyfind(module, 1, Config),
+  {_, Name} = lists:keyfind(name, 1, Config),
+  Module = sumo_config:get_prop_value(Name, module),
 
-  [First, Second | _] = sumo:find_by(Module, [{age, '>', 2}], age, 0, 0),
+  [First, Second | _] = sumo:find_by(Name, [{age, '>', 2}], age, 0, 0),
 
   <<"B">> = Module:name(First),
   <<"D">> = Module:name(Second),
 
   [First1, Second1 | _] = sumo:find_by(
-    Module,
+    Name,
     [{age, '>', 2}, {age, '=<', 5}],
     {age, desc}, 0, 0),
   <<"C">> = Module:name(First1),
   <<"D">> = Module:name(Second1),
 
-  [_, _, _] = sumo:find_by(Module, [{age, '>', 2}, {age, '=<', 5}], 0, 0),
+  [_, _, _] = sumo:find_by(Name, [{age, '>', 2}, {age, '=<', 5}], 0, 0),
   ok.
 
 -spec find_all_sort(config()) -> ok.
 find_all_sort( Config) ->
-  {_, Module} = lists:keyfind(module, 1, Config),
+  {_, Name} = lists:keyfind(name, 1, Config),
+  Module = sumo_config:get_prop_value(Name, module),
 
-  [First, Second | _] = sumo:find_all(Module, age, 0, 0),
+  [First, Second | _] = sumo:find_all(Name, age, 0, 0),
   <<"F">> = Module:name(First),
   <<"E">> = Module:name(Second),
 
   [First1, Second1 | _] = sumo:find_all(
-    Module, [{last_name, desc}, {age, asc}], 0, 0),
+    Name, [{last_name, desc}, {age, asc}], 0, 0),
   <<"F">> = Module:name(First1),
   <<"A">> = Module:name(Second1),
 
-  [First2, Second2 | _] = sumo:find_all(Module, last_name, 0, 0),
+  [First2, Second2 | _] = sumo:find_all(Name, last_name, 0, 0),
   <<"E">> = Module:name(First2),
   <<"D">> = Module:name(Second2),
   ok.
 
 -spec init_store(module()) -> ok.
-init_store(Module) ->
-  sumo:create_schema(Module),
-  sumo:delete_all(Module),
+init_store(Name) ->
+  sumo:create_schema(Name),
+  Module = sumo_config:get_prop_value(Name, module),
+  sumo:delete_all(Name),
 
-  sumo:persist(Module, Module:new(<<"A">>, <<"E">>, 6)),
-  sumo:persist(Module, Module:new(<<"B">>, <<"D">>, 3)),
-  sumo:persist(Module, Module:new(<<"C">>, <<"C">>, 5)),
-  sumo:persist(Module, Module:new(<<"D">>, <<"B">>, 4)),
-  sumo:persist(Module, Module:new(<<"E">>, <<"A">>, 2)),
-  sumo:persist(Module, Module:new(<<"F">>, <<"E">>, 1)).
+  sumo:persist(Name, Module:new(<<"A">>, <<"E">>, 6)),
+  sumo:persist(Name, Module:new(<<"B">>, <<"D">>, 3)),
+  sumo:persist(Name, Module:new(<<"C">>, <<"C">>, 5)),
+  sumo:persist(Name, Module:new(<<"D">>, <<"B">>, 4)),
+  sumo:persist(Name, Module:new(<<"E">>, <<"A">>, 2)),
+  sumo:persist(Name, Module:new(<<"F">>, <<"E">>, 1)),
+  ok.
